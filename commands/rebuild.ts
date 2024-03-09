a17d8d7fa2fee3d61d299a7ebc98e84f91795cb5
Henry-Hiles <henry@henryhiles.com>
2024-03-09 16:26:13 -0500

    Add rebuild

--- commands/rebuild.ts
+++ commands/rebuild.ts
@@ -1,5 +1,13 @@
 import { createCommand } from "commander"
+import { getConfigDir } from "../helpers/config"
+import { spawnAsync } from "@quados/helpers/cli"
 
 export default createCommand("rebuild")
     .summary("rebuild system")
-    .action(async () => console.log("todo"))
+    .action(
+        async () =>
+            await spawnAsync(`pnpm tsx config.ts`, {
+                stdio: "inherit",
+                cwd: getConfigDir(),
+            })
+    )
--- helpers/config.ts
+++ helpers/config.ts
@@ -2,6 +2,6 @@ import { homedir } from "os"
 import { join } from "path"
 
 export const getConfigDir = () =>
-    `${process.env.XDG_CONFIG_HOME ?? `${homedir()}/.config`}/quados`
+    join(process.env.XDG_CONFIG_HOME ?? join(homedir(), ".config"), "quados")
 
 export const getPackages = () => join(getConfigDir(), "packages.ts")
