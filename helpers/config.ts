680e5e10315d699318ff6b8a56e2e31d6735e3c3
Henry-Hiles <henry@henryhiles.com>
2024-03-10 18:05:12 -0400

    Remove unneeded code

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,6 +1,6 @@
 import { homedir } from "os"
 import { join } from "path"
 
-export const getConfigDir = () => join(join(homedir(), ".config"), "quados")
+export const getConfigDir = () => join(homedir(), ".config", "quados")
 
 export const getPackages = () => join(getConfigDir(), "packages.ts")
